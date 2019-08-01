024ac426b87619ecfdda29c5b6c4b1708609af49
mgoodrich <mark.goodrich@live.com>
2019-08-01 13:45:10 -0400

    add connect_timeout parameter to my.cnf and set to 28800

--- mirebalais-modules/mysql_setup/manifests/init.pp
+++ mirebalais-modules/mysql_setup/manifests/init.pp
@@ -9,6 +9,7 @@ class mysql_setup (
     $mysql_net_write_timeout = hiera('mysql_net_write_timeout'),
     $mysql_wait_timeout = hiera('mysql_wait_timeout'),
     $mysql_interactive_timeout = hiera('mysql_interactive_timeout')
+    $mysql_connect_timeout = hiera('mysql_connect_timeout')
 ){
 
 
