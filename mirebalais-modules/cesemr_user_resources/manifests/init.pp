46c34c997b1e7538c58f3b5a8f91f0c49cd68863
Brandon Istenes <bistenes@gmail.com>
2020-02-03 15:52:22 -0500

    rm -rf ces-laptop-resouces before attempting to git clone

--- mirebalais-modules/cesemr_user_resources/manifests/init.pp
+++ mirebalais-modules/cesemr_user_resources/manifests/init.pp
@@ -3,7 +3,7 @@
 class cesemr_user_resources() {
 
   exec { 'cesemr_user_resources':
-    command     => "/bin/bash -c 'cd /tmp; git clone https://github.com/PIH/ces-laptop-resources.git; cd ces-laptop-resources; cp -r CES\ EMR\ Recursos /home/doc/Escritorio/; chown doc:doc -R CES\ EMR\ Recursos'",
+    command     => "/bin/bash -c 'cd /tmp; rm -rf ces-laptop-resources; git clone https://github.com/PIH/ces-laptop-resources.git; cd ces-laptop-resources; cp -r CES\ EMR\ Recursos /home/doc/Escritorio/; chown doc:doc -R CES\ EMR\ Recursos'",
     user        => 'root'
   }
 
