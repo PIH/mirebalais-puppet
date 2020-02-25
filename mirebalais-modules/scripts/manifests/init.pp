a659cfbfca46a09eb80efc68e6a688c764e3dd22
James Mbabazi <jmbabazi@pih.org>
2020-02-25 12:06:17 -0500

    Removing the dependency

--- mirebalais-modules/scripts/manifests/init.pp
+++ mirebalais-modules/scripts/manifests/init.pp
@@ -4,16 +4,6 @@ $clean_disk_hour = hiera('clean_disk_hour')
 
 ) {
 
-cron { 'clean-disk-space':
-   ensure  => present,
-   command => '/usr/local/sbin/cleandiskspace.sh > /dev/null',
-   user    => 'root',
-   hour    => "${clean_disk_hour}",
-   minute  => 00,
-   environment => "MAILTO=${sysadmin_email}",
-   require =>  [File['cleandiskspace.sh']]
-  }
-
 file { "cleandiskspace":
    ensure => present,
    path    => '/usr/local/sbin/cleandiskspace.sh',
@@ -21,6 +11,15 @@ file { "cleandiskspace":
    owner   => 'root',
    group   => 'root',
    content => template('scripts/cleandiskspace.sh.erb'),
+  }
+
+cron { 'clean-disk-space':
+   ensure  => present,
+   command => '/usr/local/sbin/cleandiskspace.sh > /dev/null',
+   user    => 'root',
+   hour    => "${clean_disk_hour}",
+   minute  => 00,
+   environment => "MAILTO=${sysadmin_email}",
   }
 
 }
