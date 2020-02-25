da571017a9dc288f19462c535b1f4689a2f71535
James Mbabazi <jmbabazi@pih.org>
2020-02-25 14:22:21 -0500

    Disk clean up

--- mirebalais-modules/scripts/manifests/init.pp
+++ mirebalais-modules/scripts/manifests/init.pp
@@ -6,22 +6,21 @@ $sequence_directory = decrypt(hiera('sequence_folder'))
 ) {
 
 file { "cleandiskspace":
-   ensure => present,
-   path    => '/usr/local/sbin/cleandiskspace.sh',
-   mode    => '0700',
-   owner   => 'root',
-   group   => 'root',
-   content => template('scripts/cleandiskspace.sh.erb'),
+    ensure => present,
+    path    => '/usr/local/sbin/cleandiskspace.sh',
+    mode    => '0700',
+    owner   => 'root',
+    group   => 'root',
+    content => template('scripts/cleandiskspace.sh.erb'),
   }
 
 cron { 'cleandiskspace':
-   ensure  => present,
-   command => '/usr/local/sbin/cleandiskspace.sh > /dev/null 2>&1',
-   user    => 'root',
-   hour    => 19,
-   minute  => 00,
-   environment => "MAILTO=${sysadmin_email}",
-   require => [ File['cleandiskspace.sh'] ]
+    ensure  => present,
+    user    => 'root',
+    hour    => 19,
+    minute  => 20,
+    command => '/usr/local/sbin/cleandiskspace.sh >/dev/null 2>&1',
+    environment => "MAILTO=${sysadmin_email}",
   }
 
 }
