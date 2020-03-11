66df6af4f2d8c8a8b659d2fdd71fa545f364ecb5
Brandon Istenes <bistenes@gmail.com>
2020-03-11 13:40:19 -0600

    Change spa frontend zip path

--- mirebalais-modules/openmrs/manifests/spa.pp
+++ mirebalais-modules/openmrs/manifests/spa.pp
@@ -2,7 +2,7 @@ class openmrs::spa (
   $tomcat = hiera('tomcat')
 ) {
 
-  $package_url = "http://bamboo.pih-emr.org/spa-repo/unstable/pih-spa-frontend.zip"
+  $package_url = "http://bamboo.pih-emr.org/spa-repo/pih-spa-frontend/unstable/pih-spa-frontend.zip"
 
   exec{'download_and_install_spa_frontend':
     command => "/usr/bin/wget -q ${package_url} -O /tmp/pih-spa-frontend.zip && unzip -o /tmp/pih-spa-frontend.zip && rm -rf /home/${tomcat}/.OpenMRS/frontend && mv openmrs/frontend/ /home/${tomcat}/.OpenMRS/",
