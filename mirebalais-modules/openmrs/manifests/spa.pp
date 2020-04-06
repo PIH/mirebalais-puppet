d5be97be25a81a2bfd0ba3d58d9c3546fa6ab5e7
Brandon Istenes <bistenes@gmail.com>
2020-04-06 15:11:02 -0700

    fixup

--- mirebalais-modules/openmrs/manifests/spa.pp
+++ mirebalais-modules/openmrs/manifests/spa.pp
@@ -3,7 +3,14 @@ class openmrs::spa (
   $spa_ci = hiera('spa_ci'),
   $config_name = hiera('config_name')
 ) {
+  # Please see
+  # https://pihemr.atlassian.net/wiki/spaces/PIHEMR/pages/555188230/CI+Release+and+Deployment
+  # for more information about this process
 if ($spa_ci) {
+  # In CI, the import map is located on the server, but refers to modules which
+  # are served by Apache on the Bamboo server. The site-specific assets also
+  # are copied onto the server.
+
   file { "/home/${tomcat}/.OpenMRS/frontend":
     ensure => directory
   }
@@ -14,16 +21,33 @@ if ($spa_ci) {
     require => [ Package[$tomcat], File["/home/${tomcat}/.OpenMRS/frontend"] ]
   }
 
-  exec { 'add_config_file_to_import_map':
-    require =>  File["/home/${tomcat}/.OpenMRS/frontend/import-map.json"],
-    command => "sed -i 's/\"react\":/\"config-file\": \"https://github.com/PIH/${config_name}/blob/master/frontend/assets/config.json\",\n      \"react\":/' /home/${tomcat}/.OpenMRS/frontend/import-map.json",
-    onlyif  => "grep -q config-file /home/${tomcat}/.OpenMRS/frontend/import-map.json"
+  if ($config_name != '') {
+
+    exec { 'add_config_file_to_import_map':
+      require =>  File["/home/${tomcat}/.OpenMRS/frontend/import-map.json"],
+      command => "sed -i 's/\"react\":/\"config-file\": \"https://github.com/PIH/${config_name}/blob/master/frontend/assets/config.json\",\n      \"react\":/' /home/${tomcat}/.OpenMRS/frontend/import-map.json",
+      onlyif  => "grep -q config-file /home/${tomcat}/.OpenMRS/frontend/import-map.json"
+    }
+
+    $config_url = "https://github.com/PIH/${config_name}/archive/master.zip"
+
+    wget::fetch { 'download-openmrs-configuration':
+      source      => config_url,
+      destination => "/tmp/${config_name}.zip",
+      timeout     => 0,
+      verbose     => false,
+      redownload  => true,
+    }
+
+    exec{'install-openmrs-config-frontend':
+      command => "rm -rf /tmp/configuration && unzip -o /tmp/${config_name}.zip -d /tmp/configuration && rm -rf /home/${tomcat}/.OpenMRS/frontend && mkdir /home/${tomcat}/.OpenMRS/frontend && mv /tmp/frontend/* /home/${tomcat}/.OpenMRS/frontend",
+      require => [ Wget::Fetch['download-openmrs-configuration'], Package['unzip'], File["/home/${tomcat}/.OpenMRS"] ]
+    }
   }
 
-  
-
-
 } else {
+  # In a deployment, the bundle produced by Bamboo is deployed onto the server.
+
   $config_suffix = regsubst($config_name, 'openmrs-config-', '')
   $package_url = "https://bamboo.pih-emr.org:81/spa-repo/pih-spa-frontend/unstable/pih-spa-frontend-${config_suffix}.zip"
 
