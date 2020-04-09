2edb6b5bfca4242dc591356628d9b377605b7c37
mgoodrich <mark.goodrich@live.com>
2020-04-09 15:48:54 -0400

    define some dependencies between mail installs

--- mirebalais-modules/mailx/manifests/init.pp
+++ mirebalais-modules/mailx/manifests/init.pp
@@ -2,22 +2,24 @@ class mailx (
 	$smtp_username = hiera('smtp_username'),
 	$smtp_userpassword = hiera('smtp_userpassword'),
 	){
-	
-	package { 'msmtp':
-		ensure => installed,
-	}
 
-	package { 'mailutils':
+	package { 'msmtp':
 		ensure => installed,
 	}
 
 	package { 'bsd-mailx':
 		ensure => installed,
 	}
-	
+
 	package { 'msmtp-mta':
 		ensure => installed,
+    require => [ Package['msmtp'] ]
 	}
+
+  package { 'mailutils':
+    ensure => installed,
+    require => [ Package['msmtp'], Package['msmtp-mta'], Package['bsd-mailx'] ]
+  }
 
 	exec { 'msmtp info':
 	    command     => 'msmtp --serverinfo --host=smtp.gmail.com --tls=on --tls-certcheck=off',
