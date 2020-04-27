5d4c4594b847b5244e550820a3ae0b0c3679b405
Brandon Istenes <bistenes@gmail.com>
2020-04-27 14:00:32 -0700

    UHM-4490: Create versioning/release system for spa-frontend packages (#97)

--- mirebalais-modules/openmrs/manifests/spa.pp
+++ mirebalais-modules/openmrs/manifests/spa.pp
@@ -1,13 +1,13 @@
 class openmrs::spa (
   $tomcat = hiera('tomcat'),
-  $spa_ci = hiera('spa_ci'),
+  $spa_version = hiera('spa_version'),
   $config_name = hiera('config_name'),
   $webapp_name = hiera('webapp_name')
 ) {
   # Please see
   # https://pihemr.atlassian.net/wiki/spaces/PIHEMR/pages/555188230/CI+Release+and+Deployment
   # for more information about this process
-if ($spa_ci) {
+if ($spa_version == 'ci') {
   # In CI, the server uses its own local frontend/assets directory, provided
   # by the configuration repo. The server is configured to pull the import map
   # from Bamboo. That import map refers to other files hosted in Bamboo.
@@ -25,17 +25,17 @@ if ($spa_ci) {
   }
 
   file { "/home/${tomcat}/.OpenMRS/configuration/globalproperties/spa-ci-gp.xml":
-    require => [ File["/home/${tomcat}/.OpenMRS/configuration/globalproperties"] ],
     ensure  => file,
+    require => [ File["/home/${tomcat}/.OpenMRS/configuration/globalproperties"] ],
     content => template('openmrs/spa-ci-globalproperties.xml.erb'),
-    notify => [ Exec['tomcat-restart'] ]
+    notify  => [ Exec['tomcat-restart'] ]
   }
 
 } else {
   # In a deployment, the bundle produced by Bamboo is deployed onto the server.
 
   $config_suffix = regsubst($config_name, 'openmrs-config-', '')
-  $package_url = "https://bamboo.pih-emr.org:81/spa-repo/pih-spa-frontend/unstable/pih-spa-frontend-${config_suffix}.zip"
+  $package_url = "https://bamboo.pih-emr.org:81/spa-repo/pih-spa-frontend/pih-spa-${config_suffix}-${spa_version}.zip"
 
   exec{'download_and_install_spa_frontend':
     command => "/usr/bin/wget -q ${package_url} -O /tmp/pih-spa-frontend.zip && unzip -o /tmp/pih-spa-frontend.zip && rm -rf /home/${tomcat}/.OpenMRS/configuration/frontend && mv openmrs/frontend/ /home/${tomcat}/.OpenMRS/configuration/",
