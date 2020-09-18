f097fb2ddba33007127d6c1057ab7d78a6c3c6d8
James Mbabazi <jmbabazi@pih.org>
2020-09-18 10:57:23 -0400

    petl logrotate, ntp fixes, fixes to issue identified by cron

--- mirebalais-modules/ntpdate/manifests/init.pp
+++ mirebalais-modules/ntpdate/manifests/init.pp
@@ -1,6 +1,14 @@
 class ntpdate(
   $timezone = hiera('server_timezone'),
+  $server_1 = hiera('ntp_server_1'),
+  $server_2 = hiera('ntp_server_2'),
+  $server_3 = hiera('ntp_server_3'),
+  $server_4 = hiera('ntp_server_4')
 ) {
+
+  package { 'resolvconf':
+    ensure => installed,
+  }
 
   package { 'ntp':
     ensure => installed,
@@ -9,6 +17,26 @@ class ntpdate(
   package { 'ntpdate':
     ensure => installed,
     require => Package['ntp']
+  }
+
+  file { '/etc/resolvconf/resolv.conf.d/head':
+    content => template('ntpdate/head.erb'),
+    require => Package['resolvconf']
+  }
+
+  file { '/etc/resolvconf/resolv.conf.d/base':
+    content => template('ntpdate/base.erb'),
+    require => Package['resolvconf']
+  }
+
+  file { '/etc/resolv.conf':
+    content => template('ntpdate/resolv.conf.erb'),
+    require => Package['resolvconf']
+  }
+
+  exec { "initialize resolvconf":
+    command => "resolvconf -u",
+      require => [ File["/etc/resolv.conf"], File["/etc/resolvconf/resolv.conf.d/base"], File["/etc/resolvconf/resolv.conf.d/head"] ]
   }
 
   file { '/etc/ntp.conf':
@@ -38,13 +66,13 @@ class ntpdate(
     command     => 'ntpdate-debian',
     subscribe   => Exec['stop ntp'],
     refreshonly => true,
-    require => [ Package['ntp'], Package['ntpdate'] ]
+    require => [ Package['ntp'], Package['ntpdate'], Exec['initialize resolvconf'] ]
   }
 
   exec { 'start ntp':
     command     => 'service ntp start',
     subscribe   => Exec['update time'],
     refreshonly => true,
-    require => [ Package['ntp'], Package['ntpdate'] ]
+    require => [ Package['ntp'], Package['ntpdate'], Exec['initialize resolvconf'] ]
   }
 }
