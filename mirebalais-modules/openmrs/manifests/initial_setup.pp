795511f0a9c790fa541ff5c2417b9fdba0de188b
mgoodrich <mark.goodrich@live.com>
2021-05-20 14:10:45 -0400

    improve naming convention of tasks to setup schema and core data

--- mirebalais-modules/openmrs/manifests/initial_setup.pp
+++ mirebalais-modules/openmrs/manifests/initial_setup.pp
@@ -39,14 +39,14 @@ class openmrs::initial_setup(
     source => 'puppet:///modules/openmrs/liquibase.jar'
   }
 
-  openmrs::liquibase_migrate { 'migrate base schema':
+  openmrs::liquibase_migrate { 'set up base schema':
     dataset => 'liquibase-schema-only.xml',
     unless  => "mysql -u${openmrs_db_user} -p'${openmrs_db_password}' ${openmrs_db} -e 'desc patient'",
     refreshonly => true,
     notify  => Openmrs::Liquibase_migrate['migrate core data']
   }
 
-  openmrs::liquibase_migrate { 'migrate core data':
+  openmrs::liquibase_migrate { 'set up core data':
     dataset     => 'liquibase-core-data.xml',
     refreshonly => true
   }
