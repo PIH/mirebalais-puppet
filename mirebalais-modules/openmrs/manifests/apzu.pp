2668be5995b0dd8affdf3048e2d57f00cc050c4f
James Mbabazi <jmbabazi@pih.org>
2021-12-07 10:40:59 -0500

    Downloading malawi from a distro

--- mirebalais-modules/openmrs/manifests/apzu.pp
+++ mirebalais-modules/openmrs/manifests/apzu.pp
@@ -1,11 +1,12 @@
 class openmrs::apzu (
-  $tomcat_home_dir      = hiera('tomcat_home_dir'),
-  $tomcat               = hiera('tomcat'),
-  $webapp_name          = hiera('webapp_name'),
-  $openmrs_db           = hiera('openmrs_db'),
-  $openmrs_db_user      = decrypt(hiera('openmrs_db_user')),
-  $openmrs_db_password  = decrypt(hiera('openmrs_db_password')),
+  $tomcat_home_dir              = hiera('tomcat_home_dir'),
+  $tomcat                       = hiera('tomcat'),
+  $webapp_name                  = hiera('webapp_name'),
+  $openmrs_db                   = hiera('openmrs_db'),
+  $openmrs_db_user              = decrypt(hiera('openmrs_db_user')),
+  $openmrs_db_password          = decrypt(hiera('openmrs_db_password')),
   $openmrs_auto_update_database = hiera('openmrs_auto_update_database'),
+  $repo_url                     = decrypt(hiera('repo_url')),
 
 ) {
 
@@ -36,11 +37,11 @@ class openmrs::apzu (
   }
 
   exec { 'cleanup-malawi-modules-dir':
-    command => "rm -rf /tmp/malawi-modules && rm -rf /tmp/malawi-omods"
+    command => "rm -rf /tmp/malawi-modules && rm -rf /tmp/malawi-distro"
   }
 
   wget::fetch { 'download-malawi-modules':
-    source      => "https://bamboo.pih-emr.org:81/malawi-repo/modules.zip",
+    source      => "${repo_url}:81/malawi-repo/modules.zip",
     destination => "/tmp/malawi-modules",
     timeout     => 0,
     verbose     => false,
@@ -49,13 +50,19 @@ class openmrs::apzu (
   }
 
   exec { 'extract-malawi-modules':
-    command => "unzip -o /tmp/malawi-modules -d /tmp/malawi-omods && mv /tmp/malawi-omods/* /tmp/malawi-omods/modules",
+    command => "unzip -o /tmp/malawi-modules -d /tmp/malawi-distro",
     require => [ Wget::Fetch['download-malawi-modules'], Package['unzip']]
   }
 
   exec { 'move-malawi-modules':
-    command => "rm -rf ${tomcat_home_dir}/.OpenMRS/modules/* && cp -r /tmp/malawi-omods/modules/* ${tomcat_home_dir}/.OpenMRS/modules/ && chown -R ${tomcat}:${tomcat} ${tomcat_home_dir}/.OpenMRS/modules",
+    command => "rm -rf ${tomcat_home_dir}/.OpenMRS/modules/* && cp -r /tmp/malawi-distro/openmrs_modules/* ${tomcat_home_dir}/.OpenMRS/modules/ && chown -R ${tomcat}:${tomcat} ${tomcat_home_dir}/.OpenMRS/modules",
     require => [ Wget::Fetch['download-malawi-modules'], Exec['extract-malawi-modules'], File["${tomcat_home_dir}/.OpenMRS/modules"]],
+    notify => [ Exec['tomcat-restart'] ]
+  }
+
+  exec { 'move-malawi-war-file':
+    command => "rm -rf /var/lib/${tomcat}/webapps/* && cp -r /tmp/malawi-distro/openmrs_webapps/openmrs.war /var/lib/${tomcat}/webapps && chown -R ${tomcat}:${tomcat} /var/lib/${tomcat}/webapps",
+    require => [ Wget::Fetch['download-malawi-modules'], Exec['extract-malawi-modules'] ],
     notify => [ Exec['tomcat-restart'] ]
   }
 
