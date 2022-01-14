310f7efa78df6bfcbb9cd49ff2457171147ce014
James Mbabazi <jmbabazi@pih.org>
2022-01-14 16:37:11 -0500

    Install PETL DB

--- mirebalais-modules/petl/manifests/mysql.pp
+++ mirebalais-modules/petl/manifests/mysql.pp
@@ -1,20 +1,31 @@
 class petl::mysql (
   $petl_mysql_user = decrypt(hiera("petl_mysql_user")),
-  $petl_mysql_password = decrypt(hiera("petl_mysql_password"))
+  $petl_mysql_password = decrypt(hiera("petl_mysql_password")),
+  $install_petl_warehouse_db = hiera("install_petl_warehouse_db"),
+  $petl_warehouse_db = hiera("petl_warehouse_db"),
+  $petl_mysql_user_ip = hiera("petl_mysql_user_ip")
 ){
 
-  mysql_user { "${petl_mysql_user}@localhost":
+  if $install_petl_warehouse_db {
+    mysql_database { "$petl_warehouse_db":
+      ensure  => present,
+      require => [Service['mysqld']],
+      charset => 'utf8',
+    }
+  }
+
+  mysql_user { "${petl_mysql_user}@${petl_mysql_user_ip}":
     ensure        => present,
     password_hash => mysql_password($petl_mysql_password),
-    require => [ Service['mysqld']],
+    require => [Service['mysqld']],
   } ->
 
-  mysql_grant { "${petl_mysql_user}@localhost/*.*":
+  mysql_grant { "${petl_mysql_user}@${petl_mysql_user_ip}/${petl_warehouse_db}.*":
     options    => ['GRANT'],
     privileges => ['ALL'],
     table => '*.*',
-    user => "${petl_mysql_user}@localhost",
-    require => [ Service['mysqld'] ],
+    user => "${petl_mysql_user}@${petl_mysql_user_ip}",
+    require => [Service['mysqld']],
   }
 
 }
