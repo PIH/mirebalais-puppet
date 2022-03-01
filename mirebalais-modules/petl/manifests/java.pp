5288d833af01b74891b9be0bcc66980123942ef6
James Mbabazi <jmbabazi@pih.org>
2022-03-01 16:38:45 -0500

    reconfigure set-up of ces-etl on petl-test

--- mirebalais-modules/petl/manifests/java.pp
+++ mirebalais-modules/petl/manifests/java.pp
@@ -1,6 +1,11 @@
 class petl::java (
 ) {
   package { "openjdk-8-jdk":
-    ensure  => present
+    ensure => present
+  }
+
+  exec { "Hack to support multiple petl service on one server":
+    command => "cp -r /usr/lib/jvm/java-8-openjdk-amd64 /usr/lib/jvm/java-8-openjdk-amd64-2",
+    require => Package['openjdk-8-jdk'],
   }
 }
