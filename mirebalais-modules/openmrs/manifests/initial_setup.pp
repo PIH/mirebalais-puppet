7bc458955613a1163b67cee7d8b865475ddc8d62
Michael Seaton <mseaton@pih.org>
2022-05-19 18:12:45 -0400

    UHM-6469 Fix Liquibase base table and core data setup from Puppet

--- mirebalais-modules/openmrs/manifests/initial_setup.pp
+++ mirebalais-modules/openmrs/manifests/initial_setup.pp
@@ -3,6 +3,7 @@ class openmrs::initial_setup(
   $openmrs_db_user = decrypt(hiera('openmrs_db_user')),
   $openmrs_db_password = decrypt(hiera('openmrs_db_password')),
   $tomcat = hiera('tomcat'),
+  $webapp_name = hiera('webapp_name'),
   $package_name = hiera('package_name')
 ) {
 
@@ -10,13 +11,13 @@ class openmrs::initial_setup(
     ensure  => present,
     require => [Service['mysqld'],  Package[$package_name]],
     charset => 'utf8',
-  } ->
+  }
 
   mysql_user { "${openmrs_db_user}@localhost":
     ensure        => present,
     password_hash => mysql_password($openmrs_db_password),
     require => [ Service['mysqld'], Package[$package_name]],
-  } ->
+  }
 
   mysql_grant { "${openmrs_db_user}@localhost/${openmrs_db}":
     options    => ['GRANT'],
@@ -24,7 +25,7 @@ class openmrs::initial_setup(
     table => '*.*',
     user => "${openmrs_db_user}@localhost",
     require => [ Service['mysqld'],  Package[$package_name]],
-  } ->
+  }
 
   mysql_grant { "root@localhost/${openmrs_db}":
     options    => ['GRANT'],
@@ -32,7 +33,7 @@ class openmrs::initial_setup(
     table => '*.*',
     user => "root@localhost",
     require => [Service['mysqld'],  Package[$package_name]],
-    notify  => Openmrs::Liquibase_migrate['set up base schema'];
+    notify  => Exec['set up base schema'];
   }
 
   file { '/usr/local/liquibase.jar':
@@ -40,23 +41,26 @@ class openmrs::initial_setup(
     source => 'puppet:///modules/openmrs/liquibase-core-4.4.3.jar'
   }
 
-
-  openmrs::liquibase_migrate { 'set up base schema':
-    dataset => 'liquibase-schema-only.xml',
-    unless  => "mysql -u${openmrs_db_user} -p'${openmrs_db_password}' ${openmrs_db} -e 'desc patient'",
+  exec { 'set up base schema':
+    cwd         =>  '/usr/local/',
+    command     => "java -Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dliquibase.databaseChangeLogLockTableName=liquibasechangeloglock -jar liquibase.jar --driver=com.mysql.jdbc.Driver --classpath=/var/lib/${tomcat}/webapps/${webapp_name}.war --url=jdbc:mysql://localhost:3306/${openmrs_db} --changeLogFile=liquibase-schema-only.xml --username=${openmrs_db_user} --password='${openmrs_db_password}' update",
+    user        => 'root',
+    unless      => "mysql -u${openmrs_db_user} -p'${openmrs_db_password}' ${openmrs_db} -e 'desc patient'",
     refreshonly => true,
-    notify  => Openmrs::Liquibase_migrate['set up core data']
+    require     => [ File['/usr/local/liquibase.jar'], Package[$package_name] ],
+    timeout     => 0,
+    notify      => Exec['set up core data']
   }
 
-  openmrs::liquibase_migrate { 'set up core data':
-    dataset     => 'liquibase-core-data.xml',
-    refreshonly => true
+  exec { 'set up core data':
+    cwd         =>  '/usr/local/',
+    command     => "java -Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dliquibase.databaseChangeLogLockTableName=liquibasechangeloglock -jar liquibase.jar --driver=com.mysql.jdbc.Driver --classpath=/var/lib/${tomcat}/webapps/${webapp_name}.war --url=jdbc:mysql://localhost:3306/${openmrs_db} --changeLogFile=liquibase-core-data.xml --username=${openmrs_db_user} --password='${openmrs_db_password}' update",
+    user        => 'root',
+    refreshonly => true,
+    require     => [ File['/usr/local/liquibase.jar'], Package[$package_name] ],
+    timeout     => 0,
+    notify      => Exec['tomcat-restart']
   }
-
- /* openmrs::liquibase_migrate { 'migrate update to latest':
-    dataset     => 'liquibase-update-to-latest.xml',
-    refreshonly => true
-  }*/
 
   # hack to let us remote the mirebalais metadata module from the build; can be removed after it has been removed from all servers
   exec { 'make mirebalais metadata module not mandatory - error running this command can be ignored when provisioning new server':
@@ -65,10 +69,4 @@ class openmrs::initial_setup(
     require => Package[$package_name],
   }
 
-  exec { 'tomcat-start':
-    command     => "service ${tomcat} restart",
-    user        => 'root',
-    subscribe   => Openmrs::Liquibase_migrate['set up core data'],
-    refreshonly => true
-  }
 }
