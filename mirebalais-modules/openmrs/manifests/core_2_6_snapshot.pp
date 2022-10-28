d24f90f253d707e9826313990c2686ec435b5be7
Mark Goodrich <mgoodrich@pih.org>
2022-10-28 16:49:40 -0400

    UHM-6613: Set up Dispensing CI environment

--- mirebalais-modules/openmrs/manifests/core_2_6_snapshot.pp
+++ mirebalais-modules/openmrs/manifests/core_2_6_snapshot.pp
@@ -5,6 +5,7 @@ class openmrs::core_2_6_snapshot {
   wget::fetch { 'core_2_6_snapshot_download':
     source      => 'https://openmrs.jfrog.io/artifactory/snapshots/org/openmrs/web/openmrs-webapp/2.6.0-SNAPSHOT/openmrs-webapp-2.6.0-20221028.143812-235.war',
     destination => '/var/lib/tomcat7/webapps/openmrs.war',
+    redownload  => true,
     require => Package[$package_name],
     notify => Exec['tomcat-restart']
   }
