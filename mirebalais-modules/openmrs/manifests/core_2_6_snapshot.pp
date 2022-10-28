9d244f56f2a80a78f3bbfe7c7ff2585b05a2ea70
Mark Goodrich <mgoodrich@pih.org>
2022-10-28 17:16:46 -0400

    UHM-6613: Set up Dispensing CI environment

--- mirebalais-modules/openmrs/manifests/core_2_6_snapshot.pp
+++ mirebalais-modules/openmrs/manifests/core_2_6_snapshot.pp
@@ -2,13 +2,17 @@
 # temporary hack to install the current OpenMRS 2.6 core snapshot war for dispensing testing
 # note that currently hardcoded to the most recent snapshot as of Oct 28, 2022; we can change as nededed
 class openmrs::core_2_6_snapshot {
-  wget::fetch { 'core_2_6_snapshot_download':
+  wget::fetch { 'core-2_6-snapshot-download':
     source      => 'https://openmrs.jfrog.io/artifactory/snapshots/org/openmrs/web/openmrs-webapp/2.6.0-SNAPSHOT/openmrs-webapp-2.6.0-20221028.143812-235.war',
-    destination => '/var/lib/tomcat7/webapps/openmrs.war',
-    redownload  => true,
-    require => Package[$package_name],
+    destination => '/tmp/openmrs-webapp-2.6.0-20221028.143812-235.war'
+  }
+
+  exec{'core-2_6-snapshot-install':
+    command => "rm /var/lib/tomcat7/webapps/openmrs.war && rm -rf /var/lib/tomcat7/webapps/openmrs && cp /tmp/openmrs-webapp-2.6.0-20221028.143812-235.war /var/lib/tomcat7/webapps/openmrs.war",
+    require => [ Wget::Fetch['core-2_6-snapshot-download'], Package[$package_name] ],
     notify => Exec['tomcat-restart']
   }
+
 }
 
 
