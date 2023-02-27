b86155eb9b3dc578a954a9cea59c6ea873ba3f4c
James Mbabazi <jmbabazi@pih.org>
2023-02-27 10:41:49 -0500

    Draft - refactoring apzu.pp

--- mirebalais-modules/openmrs/manifests/apzu.pp
+++ mirebalais-modules/openmrs/manifests/apzu.pp
@@ -20,31 +20,8 @@ class openmrs::apzu (
 
   include openmrs::pwa
 
-  file { "${tomcat_home_dir}/.OpenMRS/${webapp_name}-runtime.properties":
-    ensure  => present,
-    content => template('openmrs/openmrs-malawi-runtime.properties.erb'),
-    owner   => $tomcat,
-    group   => $tomcat,
-    mode    => '0644',
-    require => File["${tomcat_home_dir}/.OpenMRS"]
-  }
+  include openmrs::openmrs_config_files
 
-  file { "${tomcat_home_dir}/.OpenMRS/warehouse-connection.properties":
-    ensure  => present,
-    content => template('openmrs/warehouse-connection.properties.erb'),
-    owner   => $tomcat,
-    group   => $tomcat,
-    mode    => '0600',
-    require => File["${tomcat_home_dir}/.OpenMRS"]
-  }
-
-  file { "${tomcat_home_dir}/.OpenMRS/modules":
-    ensure  => directory,
-    owner   => $tomcat,
-    group   => $tomcat,
-    mode    => '0644',
-    require => File["${tomcat_home_dir}/.OpenMRS"]
-  }
 
   exec { 'cleanup-malawi-openmrs-distribution-dir':
     command => "rm -rf /tmp/malawi-openmrs-distribution && rm -rf /tmp/malawi-distro && rm -rf /tmp/malawi-distribution"
