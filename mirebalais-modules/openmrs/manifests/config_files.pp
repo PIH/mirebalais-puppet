41849e8171ed73a94c913acd9d854568052bf39a
James Mbabazi <jmbabazi@pih.org>
2023-03-20 14:03:31 -0400

    Removing unused vars in apache pp files

--- mirebalais-modules/openmrs/manifests/config_files.pp
+++ mirebalais-modules/openmrs/manifests/config_files.pp
@@ -5,6 +5,7 @@ class openmrs::config_files (
   $config_version  = hiera('config_version'),
 
   # runtime property values
+  $openmrs_db = hiera('openmrs_db'),
   $openmrs_db_user = decrypt(hiera('openmrs_db_user')),
   $openmrs_db_password = decrypt(hiera('openmrs_db_password')),
   $openmrs_auto_update_database = hiera('openmrs_auto_update_database'),
