334d8f16376d9c1b2bb618925c24c28070d29c60
Mark Goodrich <mgoodrich@pih.org>
2023-05-09 14:19:46 -0400

    update 2.6.1 snapshot version of Core deployed to ci.pih-emr.org to most recent version

--- mirebalais-modules/openmrs/manifests/core_2_6_snapshot.pp
+++ mirebalais-modules/openmrs/manifests/core_2_6_snapshot.pp
@@ -1,14 +1,14 @@
 
 # temporary hack to install the current OpenMRS 2.6.1 core snapshot war for dispensing testing
-# note that currently hardcoded to the most recent snapshot as of Mar 07, 2023; we can change as nededed
+# note that currently hardcoded to the most recent snapshot as of Apr 25, 2023; we can change as nededed
 class openmrs::core_2_6_snapshot {
   wget::fetch { 'core-2_6-snapshot-download':
-    source      => 'https://openmrs.jfrog.io/artifactory/snapshots/org/openmrs/web/openmrs-webapp/2.6.1-SNAPSHOT/openmrs-webapp-2.6.1-20230307.115454-7.war',
-    destination => '/tmp/openmrs-webapp-2.6.1-20230307.115454-7.war'
+    source      => 'https://openmrs.jfrog.io/artifactory/snapshots/org/openmrs/web/openmrs-webapp/2.6.1-SNAPSHOT/openmrs-webapp-2.6.1-20230425.150629-16.war',
+    destination => '/tmp/openmrs-webapp-2.6.1-20230425.150629-16.war'
   }
 
   exec{'core-2_6-snapshot-install':
-    command => "rm /var/lib/tomcat7/webapps/openmrs.war && rm -rf /var/lib/tomcat7/webapps/openmrs && cp /tmp/openmrs-webapp-2.6.1-20230307.115454-7.war /var/lib/tomcat7/webapps/openmrs.war",
+    command => "rm /var/lib/tomcat7/webapps/openmrs.war && rm -rf /var/lib/tomcat7/webapps/openmrs && cp /tmp/openmrs-webapp-2.6.1-20230425.150629-16.war /var/lib/tomcat7/webapps/openmrs.war",
     require => [ Wget::Fetch['core-2_6-snapshot-download'], Package[$package_name] ],
     notify => Exec['tomcat-restart']
   }
