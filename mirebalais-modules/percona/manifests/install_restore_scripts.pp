e7f40f3177408c5fb03da9e8276e12b0065ddb40
Michael Seaton <mseaton@pih.org>
2023-07-19 08:59:01 -0400

    UHM-7279:  Use standard Percona-based backups for Haiti HIV and Haiti HIV Test

--- mirebalais-modules/percona/manifests/install_restore_scripts.pp
+++ mirebalais-modules/percona/manifests/install_restore_scripts.pp
@@ -8,23 +8,30 @@ class percona::install_restore_scripts (
     $sysadmin_email                    = hiera('sysadmin_email')
   ) {
     file { '/home/root/.percona.env':
-        ensure  => present,
-        path    => '/home/root/.percona.env',
-        mode    => '0700',
-        owner   => 'root',
-        group   => 'root',
-        content => template('percona/percona.env.erb'),
+      ensure  => present,
+      path    => '/home/root/.percona.env',
+      mode    => '0700',
+      owner   => 'root',
+      group   => 'root',
+      content => template('percona/percona.env.erb'),
+    }
+    file { "${percona_restore_dir}":
+      ensure => directory,
+      require => File["/home/root/.percona.env"]
     }
     file { '${percona_restore_dir}/percona-restore.sh':
-        ensure => present,
-        source => 'puppet:///modules/percona/percona-restore.sh',
+      ensure => present,
+      source => 'puppet:///modules/percona/percona-restore.sh',
+      require => File["${percona_restore_dir}"]
     }
     file { '${percona_restore_dir}/deidentify-db.sql':
-        ensure => present,
-        source => 'puppet:///modules/percona/deidentify-db.sql',
+      ensure => present,
+      source => 'puppet:///modules/percona/deidentify-db.sql',
+      require => File["${percona_restore_dir}/percona-restore.sh"]
     }
     file { '${percona_restore_dir}/deidentify-db.sh':
-        ensure => present,
-        source => 'puppet:///modules/percona/deidentify-db.sh',
+      ensure => present,
+      source => 'puppet:///modules/percona/deidentify-db.sh',
+      require => File["${percona_restore_dir}/deidentify-db.sql"]
     }
 }
