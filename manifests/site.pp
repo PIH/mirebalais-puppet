8eb958266fe9c1a9ed071c3d02bad5d9d5d50bee
Mark Goodrich <mgoodrich@pih.org>
2023-07-20 08:37:08 -0400

    SL-360: Create configuration for SL demo server

--- manifests/site.pp
+++ manifests/site.pp
@@ -315,7 +315,6 @@ node 'zltraining.pih-emr.org' {
   include openmrs
   include openmrs::initial_setup
 }
-
 node 'wellbody.pih-emr.org', 'kgh.pih-emr.org', 'zt-sl-kgh-inf-omrs-prod' {
 
   class { 'apt':
@@ -343,7 +342,7 @@ node 'wellbody.pih-emr.org', 'kgh.pih-emr.org', 'zt-sl-kgh-inf-omrs-prod' {
   include openmrs::backup
 }
 
-node 'kouka.pih-emr.org', 'gladi.pih-emr.org', 'inf-ami-omrs-ci', 'kgh-test.pih-emr.org', 'hinche-ci.pih-emr.org', 'zt-cloud-sis-inf-omrs-mfa-test', 'ocltest.pih-emr.org', 'sl-wellbody-inf-omrs-cc-test',
+node 'kouka.pih-emr.org', 'gladi.pih-emr.org', 'inf-ami-omrs-ci', 'sl-kgh-omrs-test.pih-emr.org', 'hinche-ci.pih-emr.org', 'zt-cloud-sis-inf-omrs-mfa-test', 'ocltest.pih-emr.org', 'sl-wellbody-inf-omrs-cc-test',
 'belladere-test.pih-emr.org' {
 
   class { 'apt':
