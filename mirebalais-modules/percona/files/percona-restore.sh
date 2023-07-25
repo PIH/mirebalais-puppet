2cd1fa822e541c196feb9752ab99f3bf2c54bf52
Michael Seaton <mseaton@pih.org>
2023-07-25 17:31:51 -0400

    UHM-7265 - Improve logging and space usage by moving instead of copying

--- mirebalais-modules/percona/files/percona-restore.sh
+++ mirebalais-modules/percona/files/percona-restore.sh
@@ -3,15 +3,20 @@
 # Restore Script for downloading a Percona backup and restoring to a MySQL instance
 # This script requires docker, azcopy, 7zip to be installed and available
 
+echoWithDate() {
+  CURRENT_DATE=$(date '+%Y-%m-%d-%H-%M-%S')
+  echo "${CURRENT_DATE}: ${1}"
+}
+
 # The following variables are expected to be present as environment variables
 # If a file exists in the user's home directory named .percona.env, source this in for these variables
 if [ -a ~/.percona.env ]; then
-  echo "Found a .percona.env file, sourcing"
+  echoWithDate "Found a .percona.env file, sourcing"
   source ~/.percona.env
 fi
 
 if [ -z "${PERCONA_RESTORE_DIR}" ] || [ -z "${PERCONA_BACKUP_PW}" ] || [ -z "${AZ_URL}" ] || [ -z "${AZ_SECRET}" ]; then
-  echo "You must have PERCONA_RESTORE_DIR, PERCONA_BACKUP_PW, AZ_URL, and AZ_SECRET environment variables defined to execute this script"
+  echoWithDate "You must have PERCONA_RESTORE_DIR, PERCONA_BACKUP_PW, AZ_URL, and AZ_SECRET environment variables defined to execute this script"
   exit 1
 fi
 
@@ -56,14 +61,14 @@ case $i in
       shift # past argument=value
     ;;
     *)
-      echo "Unknown input argument specified"
+      echoWithDate "Unknown input argument specified"
       exit 1
     ;;
 esac
 done
 
 if [ -z "$SITE_TO_RESTORE" ]; then
-    echo "You must specify the site to restore"
+    echoWithDate "You must specify the site to restore"
     exit 1
 fi
 
@@ -78,21 +83,21 @@ rm -fR ${DOWNLOAD_DIR}
 mkdir -p ${DOWNLOAD_DIR}
 mkdir -p ${DATA_DIR}
 
-echo "Starting restoration of MySQL from $SITE_TO_RESTORE"
+echoWithDate "Starting restoration of MySQL from $SITE_TO_RESTORE"
 
 if [ -z "$MYSQL_DOCKER_CONTAINER" ]; then
-    echo "Restoring into the native MySQL installation"
+    echoWithDate "Restoring into the native MySQL installation"
     MYSQL_DATA_DIR="/var/lib/mysql"
 else
-    echo "Restoring into dockerized MySQL installation, container: $MYSQL_DOCKER_CONTAINER"
+    echoWithDate "Restoring into dockerized MySQL installation, container: $MYSQL_DOCKER_CONTAINER"
     if [ -z ${MYSQL_DATA_DIR} ] || [ ${MYSQL_DATA_DIR} == '/' ]; then
       MYSQL_DATA_DIR="${BASE_DIR}/mysql"
-      echo "No MySQL Data Directory specified, defaulting to ${MYSQL_DATA_DIR}"
+      echoWithDate "No MySQL Data Directory specified, defaulting to ${MYSQL_DATA_DIR}"
     else
-      echo "MySQL Data Directory specified: ${MYSQL_DATA_DIR}"
+      echoWithDate "MySQL Data Directory specified: ${MYSQL_DATA_DIR}"
     fi
     if [ -z ${MYSQL_PORT} ]; then
-      echo "MySQL Port is required but has not been specified, exiting"
+      echoWithDate "MySQL Port is required but has not been specified, exiting"
       exit 1
     fi
 fi
@@ -105,14 +110,14 @@ copy_from_azure_percona() {
 
 copy_from_azure_percona "percona.7z.md5"
 if [ ! -f ${DOWNLOAD_DIR}/percona.7z.md5 ]; then
-    echo "Missing percona.7z.md5, exiting"
+    echoWithDate "Missing percona.7z.md5, exiting"
     exit 1
 fi
 BACKUP_MD5=$(cat ${DOWNLOAD_DIR}/percona.7z.md5)
 
 copy_from_azure_percona "percona.7z.date"
 if [ ! -f ${DOWNLOAD_DIR}/percona.7z.date ]; then
-    echo "Missing percona.7z.date, exiting"
+    echoWithDate "Missing percona.7z.date, exiting"
     exit 1
 fi
 BACKUP_DATE=$(cat ${DOWNLOAD_DIR}/percona.7z.date)
@@ -128,49 +133,49 @@ if [ -f ${STATUS_DATA_DIR}/percona.7z.date ]; then
 fi
 
 if [ "$BACKUP_MD5" = "$LAST_BACKUP_MD5" ]; then
-    echo "Current Backup MD5 matches previous backup MD5, skipping restoration"
+    echoWithDate "Current Backup MD5 matches previous backup MD5, skipping restoration"
     exit 0
 fi
 
 if [ "$BACKUP_DATE" = "$LAST_BACKUP_DATE" ]; then
-    echo "Current Backup date matches previous backup date, skipping restoration"
+    echoWithDate "Current Backup date matches previous backup date, skipping restoration"
     exit 0
 fi
 
-echo "Current backup does not match previous backup, downloading and restoring new backup"
+echoWithDate "Current backup does not match previous backup, downloading and restoring new backup"
 copy_from_azure_percona "percona.7z"
 
 DOWNLOADED_MD5=($(md5sum ${DOWNLOAD_DIR}/percona.7z))
 if [ "$DOWNLOADED_MD5" != "$BACKUP_MD5" ]; then
-    echo "Percona MD5 file MD5 of $DOWNLOADED_MD5 does not match the MD5 of the percona.7z backup of $BACKUP_MD5, exiting"
+    echoWithDate "Percona MD5 file MD5 of $DOWNLOADED_MD5 does not match the MD5 of the percona.7z backup of $BACKUP_MD5, exiting"
     exit 1
 fi
 
 # If we make it here, then we have a new, valid backup file to restore
-echo "Extracting the percona backup"
+echoWithDate "Extracting the percona backup"
 7za x ${DOWNLOAD_DIR}/percona.7z -p${PERCONA_BACKUP_PW} -y -o${DATA_DIR}
 
 if [ $? -ne 0 ]; then
-    echo "Extraction failed, exiting"
+    echoWithDate "Extraction failed, exiting"
     exit 1
 fi
 
 if [ "${RESTART_OPENMRS}" == "true" ]; then
-  echo "Stopping Tomcat"
+  echoWithDate "Stopping Tomcat"
   /etc/init.d/${TOMCAT_USER} stop
 
-  echo "Removing configuration checksums and lib cache"
+  echoWithDate "Removing configuration checksums and lib cache"
   rm -rf ${TOMCAT_HOME_DIR}/.OpenMRS/.openmrs-lib-cache
   rm -rf ${TOMCAT_HOME_DIR}/.OpenMRS/configuration_checksums
 fi
 
-echo "Stopping the existing MySQL instance"
+echoWithDate "Stopping the existing MySQL instance"
 
 if [ -z "$MYSQL_DOCKER_CONTAINER" ]; then
-    echo "Stopping native mysql"
+    echoWithDate "Stopping native mysql"
     service mysql stop
 else
-    echo "Re-creating MySQL container: $MYSQL_DOCKER_CONTAINER"
+    echoWithDate "Re-creating MySQL container: $MYSQL_DOCKER_CONTAINER"
     docker stop $MYSQL_DOCKER_CONTAINER || true
     docker rm $MYSQL_DOCKER_CONTAINER || true
     docker run --name ${MYSQL_DOCKER_CONTAINER} \
@@ -183,76 +188,68 @@ else
         --max_allowed_packet=1G \
         --innodb-buffer-pool-size=2G
     sleep 10
-    echo "Stopping MySQL container"
+    echoWithDate "Stopping MySQL container"
     docker stop $MYSQL_DOCKER_CONTAINER || true
 fi
 
-echo "Re-creating existing data directory: ${MYSQL_DATA_DIR}"
+echoWithDate "Re-creating existing data directory: ${MYSQL_DATA_DIR}"
 rm -fR ${MYSQL_DATA_DIR}
 mkdir -p ${MYSQL_DATA_DIR}
 
-echo "Copying the percona backup contents into the MySQL data directory"
+echoWithDate "Moving the percona backup contents into the MySQL data directory"
 docker run --name percona --rm \
     -v ${DATA_DIR}:/opt/backup \
     -v ${MYSQL_DATA_DIR}:/var/lib/mysql \
     partnersinhealth/percona-0.1-4 \
-    innobackupex --copy-back --datadir=/var/lib/mysql /opt/backup
+    innobackupex --move-back --datadir=/var/lib/mysql /opt/backup
 
 if [ -z "$MYSQL_DOCKER_CONTAINER" ]; then
-    echo "Changing permissions of data directory"
+    echoWithDate "Changing permissions of data directory"
     chown -R mysql:mysql ${MYSQL_DATA_DIR}
-    echo "Starting native mysql"
+    echoWithDate "Starting native mysql"
     /etc/init.d/mysql start
     sleep 10
-    echo "Running mysql check"
+    echoWithDate "Running mysql check"
     /usr/bin/mysqlcheck --auto-repair --check --all-databases -ubackup -p${PERCONA_BACKUP_PW}
 else
-    echo "Changing permissions of data directory"
+    echoWithDate "Changing permissions of data directory"
     chown -R 999:999 ${MYSQL_DATA_DIR}
-    echo "Starting MySQL container: $MYSQL_DOCKER_CONTAINER"
+    echoWithDate "Starting MySQL container: $MYSQL_DOCKER_CONTAINER"
     docker start $MYSQL_DOCKER_CONTAINER
     sleep 10
-    echo "Running mysql check"
+    echoWithDate "Running mysql check"
     docker exec -i $MYSQL_DOCKER_CONTAINER sh -c "/usr/bin/mysqlcheck --auto-repair --check --all-databases -ubackup -p'${PERCONA_BACKUP_PW}'"
 fi
 
 if [ $? -eq 0 ]; then
-    echo "Backup restored successfully and all tables are correct"
-
-    # Once backup restored successfully, copy the latest MD5 and date files into the status directory and delete downloaded files
-    echo "Backup restoration successful, copying most recent backup date and md5 files into status directory"
-    cp ${DOWNLOAD_DIR}/percona.7z.date ${STATUS_DATA_DIR}/
-    cp ${DOWNLOAD_DIR}/percona.7z.md5 ${STATUS_DATA_DIR}/
-    rm -fR ${DOWNLOAD_DIR}
-    echo "Successfully completed"
-
+    echoWithDate "Backup restored successfully and all tables are correct"
 else
-    echo "Mysql check failed, exiting"
+    echoWithDate "Mysql check failed, exiting"
     exit 1
 fi
 
 if [ "${DEIDENTIFY}" == "true" ]; then
-  echo "De-identifying the database"
+  echoWithDate "De-identifying the database"
   if [ -z "${MYSQL_DOCKER_CONTAINER}" ]; then
-      echo "De-identifying the native MySQL installation"
+      echoWithDate "De-identifying the native MySQL installation"
       mysql -uroot -p${MYSQL_ROOT_PW} openmrs < ${PERCONA_RESTORE_DIR}/deidentify-db.sql
   else
-      echo "De-identifying dockerized MySQL installation, container: ${MYSQL_DOCKER_CONTAINER}"
+      echoWithDate "De-identifying dockerized MySQL installation, container: ${MYSQL_DOCKER_CONTAINER}"
       docker exec -i ${MYSQL_DOCKER_CONTAINER} mysql -uroot -p${MYSQL_ROOT_PW} openmrs < ${PERCONA_RESTORE_DIR}/deidentify-db.sql
   fi
   if [ $? -eq 0 ]; then
-      echo "De-identification successful"
+      echoWithDate "De-identification successful"
   else
-    echo "An error occurred during de-identification"
+    echoWithDate "An error occurred during de-identification"
     exit 1
   fi
 fi
 
 if [ "${CREATE_PETL_USER}" == "true" ]; then
-  echo "Creating PETL user"
+  echoWithDate "Creating PETL user"
 
   if [ -z "${PETL_MYSQL_USER}" ] || [ -z "${PETL_MYSQL_USER_IP}" ] || [ -z "${PETL_MYSQL_PASSWORD}" ] || [ -z "${PETL_OPENMRS_DB}" ]; then
-    echo "You must have PETL_MYSQL_USER, PETL_MYSQL_USER_IP, PETL_MYSQL_PASSWORD, PETL_OPENMRS_DB environment variables defined to create petl user"
+    echoWithDate "You must have PETL_MYSQL_USER, PETL_MYSQL_USER_IP, PETL_MYSQL_PASSWORD, PETL_OPENMRS_DB environment variables defined to create petl user"
     exit 1
   fi
 
@@ -261,28 +258,40 @@ if [ "${CREATE_PETL_USER}" == "true" ]; then
   GRANT_USER_SQL="grant all privileges on ${PETL_OPENMRS_DB}.* to '${PETL_MYSQL_USER}'@'${PETL_MYSQL_USER_IP}';"
 
   if [ -z "$MYSQL_DOCKER_CONTAINER" ]; then
-      echo "Creating PETL DB user is only currently supported in Docker, exiting"
+      echoWithDate "Creating PETL DB user is only currently supported in Docker, exiting"
       exit 1
   else
-      echo "Ensuring MySQL user ${PETL_MYSQL_USER}'@'${PETL_MYSQL_USER_IP}' in container ${MYSQL_DOCKER_CONTAINER}"
+      echoWithDate "Ensuring MySQL user ${PETL_MYSQL_USER}'@'${PETL_MYSQL_USER_IP}' in container ${MYSQL_DOCKER_CONTAINER}"
       EXISTING_USERS=$(docker exec -i ${MYSQL_DOCKER_CONTAINER} mysql -u root -p${MYSQL_ROOT_PW} -N -e "${SELECT_USER_SQL}")
       if [ "${EXISTING_USERS}" -eq 0 ]; then
-        echo "No user found, creating"
+        echoWithDate "No user found, creating"
         docker exec -i ${MYSQL_DOCKER_CONTAINER} mysql -u root -p${MYSQL_ROOT_PW} -e "${CREATE_USER_SQL} ${GRANT_USER_SQL}"
         if [ $? -eq 0 ]; then
-          echo "Create user successful"
+          echoWithDate "Create user successful"
         else
-          echo "Create user failed, exiting"
+          echoWithDate "Create user failed, exiting"
           exit 1
         fi
       else
-        echo "User already exists, not re-creating"
+        echoWithDate "User already exists, not re-creating"
       fi
   fi
 
 fi
 
+if [ $? -eq 0 ]; then
+  # If the script makes it here, copy the latest MD5 and date files into the status directory and delete downloaded files
+  echoWithDate "Backup restoration successful, copying most recent backup date and md5 files into status directory"
+  cp ${DOWNLOAD_DIR}/percona.7z.date ${STATUS_DATA_DIR}/
+  cp ${DOWNLOAD_DIR}/percona.7z.md5 ${STATUS_DATA_DIR}/
+  rm -fR ${DOWNLOAD_DIR}
+  echoWithDate "Successfully completed"
+else
+  echoWithDate "Restoration failed"
+  exit 1
+fi
+
 if [ "${RESTART_OPENMRS}" == "true" ]; then
-  echo "Starting Tomcat"
+  echoWithDate "Starting Tomcat"
   /etc/init.d/${TOMCAT_USER} start
 fi
