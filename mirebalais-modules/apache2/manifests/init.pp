ad853124159ae8277f71961321e6e6be61301077
James Gihana <jmbabazi@pih.org>
2023-09-25 16:57:24 -0400

    Creating ACME user: Ensure cron is executed by acme user

--- mirebalais-modules/apache2/manifests/init.pp
+++ mirebalais-modules/apache2/manifests/init.pp
@@ -169,7 +169,7 @@ class apache2 (
   cron { "renew certificates using acme user":
     ensure  => present,
     command => "'/var/$acme_user/.acme.sh'/acme.sh --cron --home '/var/$acme_user/.acme.sh' > /dev/null",
-    user    => "root",
+    user    => "$acme_user",
     hour    => "$cert_cron_hour",
     minute  => "$cert_cron_min",
     environment => "MAILTO=$sysadmin_email",
