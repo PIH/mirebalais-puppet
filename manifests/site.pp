38b8b1a79f320925e6ae422251bc1e7ba6f4c28b
Michael Seaton <mseaton@pih.org>
2023-11-10 00:01:07 -0500

    UHM-7528 Fix/Improve pipeline copying database from HUM Production to HUM Reporting

--- manifests/site.pp
+++ manifests/site.pp
@@ -270,7 +270,8 @@ node 'hai-hum-inf-omrs-report' {
   include docker
   include percona::install_restore_scripts
   class { 'percona::setup_cron_to_refresh_openmrs_db':
-    site_name => 'haiti/mirebalais'
+    site_name => 'haiti/mirebalais',
+    percona_restore_deidentify => false
   }
 }
 
