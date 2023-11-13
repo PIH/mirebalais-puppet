64195a4d4ffc8112b5c0302858353262337117da
Michael Seaton <mseaton@pih.org>
2023-11-12 22:12:43 -0500

    UHM-7528 Bug fixes

--- manifests/percona.pp
+++ manifests/percona.pp
@@ -1,7 +1,6 @@
 Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin/' ] }
 
 node 'hai-hum-inf-omrs-report' {
-  include docker
   include percona::install_restore_scripts
   class { 'percona::setup_cron_to_refresh_openmrs_db':
     site_name => 'haiti/mirebalais',
