5cc8ff4fb9f373b0812bbfe7e874efe2ac6bd545
Michael Seaton <mseaton@pih.org>
2024-02-02 17:32:41 -0500

    Second pass at commcare sync setup

--- mirebalais-modules/commcare_sync/manifests/init.pp
+++ mirebalais-modules/commcare_sync/manifests/init.pp
@@ -1,55 +1,87 @@
 class commcare_sync(
-  $commcare_sync_user = "commcare_sync",
-  $commcare_sync_home_dir = "/home/${commcare_sync_user}",
-  $commcare_sync_code_dir = "${commcare_sync_home_dir}/code",
+  $commcare_sync_app_name = "commcare_sync",
+  $container_dir = "/container",
+  $commcare_sync_app_dir = "${container_dir}/${commcare_sync_app_name}",
+  $dockerdata_dir = "/dockerdata",
+  $commcare_sync_data_dir = "${$dockerdata_dir}/${commcare_sync_app_name}",
+  $commcare_sync_postgres_data_dir = "${commcare_sync_data_dir}/postgres",
+  $commcare_sync_redis_data_dir = "${commcare_sync_data_dir}/redis",
+  $commcare_sync_postgres_password = decrypt(hiera('commcare_sync_postgres_password')),
+  $commcare_sync_secret_key = decrypt(hiera('commcare_sync_secret_key')),
+  $commcare_sync_code_dir = "${commcare_sync_app_dir}/code",
   $commcare_sync_github_url = "https://github.com/dimagi/commcare-sync.git",
+  $sysadmin_email = decrypt(hiera('sysadmin_email')),
 ) {
 
-    user { "${commcare_sync_user}":
-        ensure => "present",
-        home   => "${commcare_sync_home_dir}",
-        groups => "docker",
-        shell  => "/bin/bash"
+    file { "${container_dir}":
+        ensure  => directory,
+        mode    => "0755",
     }
 
-    file { "${commcare_sync_home_dir}":
+    file { "${commcare_sync_app_dir}":
         ensure  => directory,
-        owner   => "${commcare_sync_user}",
-        group   => "${commcare_sync_user}",
         mode    => "0755",
-        require => User["${commcare_sync_user}"]
+        require => File["${container_dir}"]
+    }
+
+    file { "${dockerdata_dir}":
+        ensure  => directory,
+        mode    => "0755",
+        require => File["${commcare_sync_app_dir}"]
+    }
+
+    file { "${commcare_sync_data_dir}":
+        ensure  => directory,
+        mode    => "0755",
+        require => File["${dockerdata_dir}"]
+    }
+
+    file { "${commcare_sync_postgres_data_dir}":
+        ensure  => directory,
+        mode    => "0600",
+        require => File["${commcare_sync_data_dir}"]
+    }
+
+    file { "${commcare_sync_redis_data_dir}":
+        ensure  => directory,
+        mode    => "0600",
+        require => File["${commcare_sync_data_dir}"]
     }
 
     vcsrepo { "${commcare_sync_code_dir}":
         provider => git,
         source   => "${commcare_sync_github_url}",
         ensure   => latest,
-        require => File["${commcare_sync_home_dir}"]
+        require => File["${commcare_sync_redis_data_dir}"]
     }
 
-    file { "${commcare_sync_home_dir}/.commcare_sync.env":
+    file { "${commcare_sync_code_dir}/localsettings.py":
         ensure  => present,
-        path    => "${commcare_sync_home_dir}/.commcare_sync.env",
+        path    => "${commcare_sync_code_dir}/localsettings.py",
         mode    => "0700",
-        owner   => "${commcare_sync_user}",
-        group   => "${commcare_sync_user}",
-        content => template('commcare_sync/commcare_sync.env.erb'),
-        require => File["${commcare_sync_home_dir}"]
+        content => template('commcare_sync/localsettings.py.erb'),
+        require => Vcsrepo["${commcare_sync_code_dir}"]
     }
 
-    file { "${commcare_sync_home_dir}/setup.sh":
+    file { "${commcare_sync_app_dir}/docker-compose.yml":
+        ensure  => present,
+        path    => "${commcare_sync_app_dir}/docker-compose.yml",
+        mode    => "0700",
+        content => template('commcare_sync/docker-compose.yml.erb'),
+        require => File["${commcare_sync_code_dir}/localsettings.py"]
+    }
+
+    file { "${commcare_sync_app_dir}/setup.sh":
       ensure => present,
-      source => "puppet:///modules/commcare_sync/setup.sh",
-      path    => "${commcare_sync_home_dir}/setup.sh",
+      path    => "${commcare_sync_app_dir}/setup.sh",
       mode    => "0700",
-      owner   => "${commcare_sync_user}",
-      group   => "${commcare_sync_user}",
-      require => File["${commcare_sync_home_dir}/.commcare_sync.env"]
+      content => template('commcare_sync/setup.sh.erb'),
+      require => File["${commcare_sync_app_dir}/docker-compose.yml"]
     }
 
-    exec { "${commcare_sync_home_dir}/setup.sh":
-        command => "bash setup.sh ${commcare_sync_code_dir}",
-        cwd   => "${commcare_sync_home_dir}",
-        require => File["${commcare_sync_home_dir}/setup.sh"]
+    exec { "${commcare_sync_app_dir}/setup.sh":
+        command => "bash setup.sh",
+        cwd   => "${commcare_sync_app_dir}",
+        require => File["${commcare_sync_app_dir}/setup.sh"]
     }
 }
