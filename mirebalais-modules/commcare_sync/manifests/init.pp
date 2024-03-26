5bead85d63ac458e35398f48841edfb4160be8f2
Michael Seaton <mseaton@pih.org>
2024-03-26 10:10:44 -0400

    UHM-7683 - Change commcare sync to use a published docker container

--- mirebalais-modules/commcare_sync/manifests/init.pp
+++ mirebalais-modules/commcare_sync/manifests/init.pp
@@ -9,8 +9,6 @@ class commcare_sync(
   $commcare_sync_admin_password = decrypt(hiera('commcare_sync_admin_password')),
   $commcare_sync_postgres_password = decrypt(hiera('commcare_sync_postgres_password')),
   $commcare_sync_secret_key = decrypt(hiera('commcare_sync_secret_key')),
-  $commcare_sync_code_dir = "${commcare_sync_app_dir}/code",
-  $commcare_sync_github_url = "https://github.com/dimagi/commcare-sync.git",
   $sysadmin_email = decrypt(hiera('sysadmin_email')),
 ) {
 
@@ -49,19 +47,12 @@ class commcare_sync(
         require => File["${commcare_sync_data_dir}"]
     }
 
-    vcsrepo { "${commcare_sync_code_dir}":
-        provider => git,
-        source   => "${commcare_sync_github_url}",
-        ensure   => latest,
-        require => File["${commcare_sync_redis_data_dir}"]
-    }
-
-    file { "${commcare_sync_code_dir}/localsettings.py":
+    file { "${commcare_sync_app_dir}/localsettings.py":
         ensure  => present,
-        path    => "${commcare_sync_code_dir}/localsettings.py",
+        path    => "${commcare_sync_app_dir}/localsettings.py",
         mode    => "0700",
         content => template('commcare_sync/localsettings.py.erb'),
-        require => Vcsrepo["${commcare_sync_code_dir}"]
+        require => File["${commcare_sync_app_dir}"]
     }
 
     file { "${commcare_sync_app_dir}/docker-compose.yml":
@@ -69,7 +60,7 @@ class commcare_sync(
         path    => "${commcare_sync_app_dir}/docker-compose.yml",
         mode    => "0700",
         content => template('commcare_sync/docker-compose.yml.erb'),
-        require => File["${commcare_sync_code_dir}/localsettings.py"]
+        require => File["${commcare_sync_app_dir}/localsettings.py"]
     }
 
     file { "${commcare_sync_app_dir}/setup.sh":
